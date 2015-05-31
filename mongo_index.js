048663446c68a3038092ef5f32e55c37bae40149
yanyiwu <i@yanyiwu.com>
2015-05-31 12:03:33 +0800

    mongo index

--- mongo_index.js
+++ mongo_index.js
@@ -1,5 +1,7 @@
-use inforyoumation
-db.feeds.ensureIndex({"urlmd5":1},{"unique":true})
-db.keyword.ensureIndex({"keyword":1, "feedid":1},{"unique":true})
-db.feeds.ensureIndex({"lastmodified":-1}, {"expireAfterSeconds": 3600})
-db.keyword.ensureIndex({"lastmodified":-1}, {"expireAfterSeconds": 3600})
+use inforyoumation;
+//db.dropDatabase();
+var expireSeconds = 3600*24*7;
+db.feeds.ensureIndex({"urlmd5":1},{"unique":true});
+db.keyword.ensureIndex({"keyword":1, "feedid":1},{"unique":true});
+db.feeds.ensureIndex({"lastmodified":-1}, {"expireAfterSeconds": expireSeconds});
+db.keyword.ensureIndex({"lastmodified":-1}, {"expireAfterSeconds": expireSeconds});
