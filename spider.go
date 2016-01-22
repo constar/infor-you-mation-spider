c005b7d996323bf718e8b28581512ae71b5bf56c
yanyiwu <i@yanyiwu.com>
2016-01-23 00:56:12 +0800

    add flags

--- spider.go
+++ spider.go
@@ -9,46 +9,53 @@ import (
 	"github.com/yanyiwu/igo"
 )
 
-const (
-	SleepSeconds = 60
-)
+var sleepSeconds = flag.Int("sleep", 60, "sleep seconds")
+var isForever = flag.Bool("forever", false, "run forever")
 
 var wg sync.WaitGroup
 
+func Crawl(url string) {
+	content := igo.HttpGet(url)
+	content = convert(content)
+	msgs := Parse(content)
+	if msgs == nil {
+		glog.Error("Parse failed")
+	} else {
+		for _, item := range msgs {
+			glog.V(3).Info(item)
+			j := Job{
+				item.GetTitle(),
+				item.GetContent(),
+				item.GetUrl(),
+				igo.GetMd5String(item.GetUrl()),
+			}
+			jobid, err := SaveJob(j)
+			if err != nil {
+				glog.V(2).Info(err)
+			} else {
+				Dispatch(item.GetTitle(), jobid)
+			}
+			//oid, err := Insert("feeds", item.GetTitle(), item.GetContent(), item.GetUrl())
+			//if err == nil {
+			//	glog.Info(item.GetTitle(), " ", item.GetUrl())
+			//	Dispatch(item.GetTitle(), oid)
+			//} else {
+			//	glog.V(2).Info(err)
+			//}
+		}
+	}
+}
+
 func spiderRunner(url string) {
 	defer wg.Done()
-	for {
-		content := igo.HttpGet(url)
-		content = convert(content)
-		msgs := Parse(content)
-		if msgs == nil {
-			glog.Error("Parse failed")
-		} else {
-			for _, item := range msgs {
-				glog.V(3).Info(item)
-				j := Job{
-					item.GetTitle(),
-					item.GetContent(),
-					item.GetUrl(),
-					igo.GetMd5String(item.GetUrl()),
-				}
-				jobid, err := SaveJob(j)
-				if err != nil {
-					glog.V(2).Info(err)
-				} else {
-					Dispatch(item.GetTitle(), jobid)
-				}
-				//oid, err := Insert("feeds", item.GetTitle(), item.GetContent(), item.GetUrl())
-				//if err == nil {
-				//	glog.Info(item.GetTitle(), " ", item.GetUrl())
-				//	Dispatch(item.GetTitle(), oid)
-				//} else {
-				//	glog.V(2).Info(err)
-				//}
-			}
+	if *isForever {
+		for {
+			Crawl(url)
+			glog.V(3).Info("time.Sleep ", *sleepSeconds, " seconds")
+			time.Sleep(time.Duration(*sleepSeconds) * time.Second)
 		}
-		glog.V(3).Info("time.Sleep ", SleepSeconds, " seconds")
-		time.Sleep(SleepSeconds * time.Second)
+	} else {
+		Crawl(url)
 	}
 }
 
